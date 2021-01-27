d60c74ba7aa12a57b81e4244315fbe99c4be9ca9
seanb <sesnbarzilay@gmail.com>
2021-01-27 22:31:03 +0200

    Support Multiple Controllers

--- Receiver.py
+++ Receiver.py
@@ -9,6 +9,7 @@ class Receiver:
     port = 8123
     address = None
     joy = None
+    joys = {}
 
     def __init__(self, group) -> None:
         import pyvjoy
@@ -27,15 +28,22 @@ class Receiver:
         else:
             mreq = group_bin + struct.pack('@I', 0)
             s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
-        j = pyvjoy.VJoyDevice(1)
+        # j = pyvjoy.VJoyDevice(1)
         Receiver.sock = s
-        Receiver.joy = j
+        # Receiver.joy = j
         last_pos = (0, 0)
         middle = 0x8000 / 2
-        j.set_axis(pyvjoy.HID_USAGE_X, int(middle))
-        j.set_axis(pyvjoy.HID_USAGE_Y, int(middle))
+
         while True:  # TODO Remove infinite loop
             data, sender = Receiver.read_data()
+            if sender in Receiver.joys:
+                j = Receiver.joys[sender]
+            else:
+                print(f"New Connection: {sender}")
+                j = pyvjoy.VJoyDevice(len(Receiver.joys) + 1)
+                j.set_axis(pyvjoy.HID_USAGE_X, int(middle))
+                j.set_axis(pyvjoy.HID_USAGE_Y, int(middle))
+                Receiver.joys[sender] = j  # TODO: Think about when to "close" connection
             event, actions = data.split(" ")  # TODO: change to pattern matching with python3.10
             if event == "press" or event == "release":
                 if len(actions) > 1:
@@ -65,7 +73,7 @@ class Receiver:
                 j.set_button(key, event)
             elif event == "moved":
                 x, y = actions.split(',')
-                pos = (int(x), int(y))
+                pos = (int(x.split('.')[0]), int(y.split('.')[0]))
                 dx = pos[0] - last_pos[0]
                 dy = pos[1] - last_pos[1]
                 j.set_axis(pyvjoy.HID_USAGE_X, int(middle + int(str(int(dx) * 100), 16)))
