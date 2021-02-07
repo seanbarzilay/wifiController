d94b71e96fd5377d3197f609450b50c571db970d
seanb <sesnbarzilay@gmail.com>
2021-02-07 23:47:44 +0200

    Cleaning

--- wifi_controller/receivers/GamepadReceiver.py
+++ wifi_controller/receivers/GamepadReceiver.py
@@ -37,7 +37,7 @@ class GamepadReceiver(Receiver):
                     'LEFT': ["Dpad", c_int, 4],
                     'RIGHT': ["Dpad", c_int, 8],
                     'START': ["BtnStart", c_bool, 1],
-                    'SELECT': ["BtnStart", c_bool, 1],
+                    'SELECT': ["BtnBack", c_bool, 1],
                     'L3': ["BtnThumbL", c_bool, 1],
                     'R3': ["BtnThumbR", c_bool, 1],
                     'LB': ["BtnX", c_bool, 1],
@@ -55,94 +55,6 @@ class GamepadReceiver(Receiver):
                 }
             logging.info(data)
             state = json.loads(data.replace("'", '"'))
+            state['Lx'] = self.translate(state['Lx'], 0, 26256, -1, 1)
+            state['Ly'] = -1 * self.translate(state['Ly'], 0, 26256, -1, 1)
             con.playMoment(state)
-
-        # import pyvjoy
-        # middle = 0x8000 / 2
-        # min = -800
-        # max = 840
-        # last_pos = (0, 0)
-        # while True:  # TODO Remove infinite loop
-        #     data, sender = Receiver.read_data()
-        #     if sender in self.joys:
-        #         j = self.joys[sender]
-        #     else:
-        #         logging.info(f"New Connection: {sender}, {len(self.joys) + 1}")
-        #         j = pyvjoy.VJoyDevice(len(self.joys) + 1)
-        #         logging.info(f"{pyvjoy.HID_USAGE_X}, {int(middle)}")
-        #         j.set_axis(pyvjoy.HID_USAGE_X, int(middle))
-        #         j.set_axis(pyvjoy.HID_USAGE_Y, int(middle))
-        #         self.joys[sender] = j  # TODO: Think about when to "close" connection
-        #     if data == "goodbye":
-        #         logging.info("bye bye")
-        #         self.joys[sender].__del__()
-        #         del self.joys[sender]
-        #         continue
-        #     # {2: 0, 4: 0, 1: 0, 3: 0, 9: 0, 10: 0, 'left': (13128.0, 13128.0)}
-        #     j.update()
-        #     event, actions = data.split(" ")  # TODO: change to pattern matching with python3.10
-        #     if event == "press" or event == "release":
-        #         # if len(actions) > 1:
-        #         #     key = Key[actions]
-        #         # else:
-        #         key = actions
-        #         event = 1 if event == "press" else 0
-        #         # if key == Key.up:
-        #         #     key = 13
-        #         # if key == Key.down:
-        #         #     key = 14
-        #         # if key == Key.left:
-        #         #     key = 15
-        #         # if key == Key.right:
-        #         #     key = 16
-        #         # else:
-        #         try:
-        #             key = int(key)
-        #             if key == 0:
-        #                 continue
-        #         except TypeError as e:
-        #             logging.info(e)
-        #             continue
-        #         except ValueError as e:
-        #             logging.info(e)
-        #             continue
-        #         j.set_button(key, event)
-        #     elif event == "moved":
-        #         x, y = actions.split(',')
-        #         x = float(x)
-        #         y = float(y)
-        #         if x <= -400:  # left
-        #             j.set_button(15, 1)
-        #         elif x >= 400:  # right
-        #             j.set_button(16, 1)
-        #         else:
-        #             j.set_button(15, 0)
-        #             j.set_button(16, 0)
-        #         if y <= -400:  # up
-        #             j.set_button(13, 1)
-        #         elif y >= 400:  # down
-        #             j.set_button(14, 1)
-        #         else:
-        #             j.set_button(13, 0)
-        #             j.set_button(14, 0)
-        #         # pos = (int(x.split('.')[0]), int(y.split('.')[0]))
-        #         # pos = (self.translate(pos[0], -800, 840, min, max), self.translate(pos[1], -800, 840, min, max))
-        #         # # dx = pos[0] - last_pos[0]
-        #         # # dy = pos[1] - last_pos[1]
-        #         # logging.info(pos)
-        #         # j.set_axis(pyvjoy.HID_USAGE_X, int(middle + (int(pos[0]) * 100)))
-        #         # j.set_axis(pyvjoy.HID_USAGE_Y, int(middle + (int(pos[1]) * 100)))
-        #
-        #         # last_pos = pos
-        #     elif event == "clicked":
-        #         button, pressed, x, y = actions.split(',')
-        #         if pressed == "True":
-        #             if button == 'left':
-        #                 j.set_button(7, 1)
-        #             else:
-        #                 j.set_button(8, 1)
-        #         else:
-        #             if button == 'left':
-        #                 j.set_button(7, 0)
-        #             else:
-        #                 j.set_button(8, 0)
