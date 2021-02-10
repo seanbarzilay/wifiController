c451c9eb0de26928bb8732f779656c6933902e2c
seanb <sesnbarzilay@gmail.com>
2021-02-10 21:06:37 +0200

    move audit to thread

--- wifi_controller/core/EventAuditer.py
+++ wifi_controller/core/EventAuditer.py
@@ -1,11 +1,14 @@
 import logging
 import requests
+import _thread
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
 def audit_event(path: str, event: dict) -> None:
-    res = requests.post(f"http://seans-pc:9200/{path}/_doc", json=event)
-    print(repr(res))
-    if res.status_code > 201:
-        logging.debug(repr(res))
+    def audit():
+        res = requests.post(f"http://seans-pc:9200/{path}/_doc", json=event)
+        print(repr(res))
+        if res.status_code > 201:
+            logging.debug(repr(res))
+    _thread.start_new_thread(audit, ())
