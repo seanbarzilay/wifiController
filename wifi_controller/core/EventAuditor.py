5f0d4e0d0c497204682e3d1f9b6aef7ded3b06be
seanb <sesnbarzilay@gmail.com>
2021-02-13 22:32:16 +0200

    add new line

--- wifi_controller/core/EventAuditor.py
+++ wifi_controller/core/EventAuditor.py
@@ -6,5 +6,5 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 def audit_event(path: str, event: dict) -> None:
     with open(path, 'a') as f:
-        f.write(str(event))
+        f.write(str(event) + '\n')
 
