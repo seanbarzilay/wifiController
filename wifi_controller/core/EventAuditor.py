42619fee75e547984824d2a487d9d507190e4509
seanb <sesnbarzilay@gmail.com>
2021-02-13 22:22:22 +0200

    change auditor to append

--- wifi_controller/core/EventAuditor.py
+++ wifi_controller/core/EventAuditor.py
@@ -5,6 +5,6 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
 def audit_event(path: str, event: dict) -> None:
-    with open(path, 'wa') as f:
+    with open(path, 'a') as f:
         f.write(str(event))
 
